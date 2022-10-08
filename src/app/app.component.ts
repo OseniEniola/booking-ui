ab0a0b5e0e65d4b0c38863d9dfe236c844188b55
Michael Ovie <mikaelovi@gmail.com>
2022-10-08 12:03:42 +0100

    Booking UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild,ViewEncapsulation  } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'booking-ui';
-  isDark =false;
+  isLight =true;
+
+  @ViewChild('ngcarousel', { static: true }) ngCarousel!: NgbCarousel;
+  // Move to previous slide
+  getToPrev() {
+    this.ngCarousel.prev();
+  }
+  // Move to next slide
+  goToNext() {
+    this.ngCarousel.next();
+  }
 }
